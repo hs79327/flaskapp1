8ae74aab83669ae5513915a5523406fab789139e
harshal-dev1 <harshal-dev1@demo.lab>
2024-12-26 16:11:59 +0530

    hello.py again modified

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Welcome to the comedy class!!! Bapat is doing comedy with dhanno by calling him meetha"
+    return "I am bored.... tomorrow I will hire jenkins!!!"
 if __name__ == '__main__':
      app.run(host='0.0.0.0', port='7000', debug=True)
 
