a5a5639bdc56bc88337999cdf6307bb9620ade22
harshal-dev1 <harshal-dev1@demo.lab>
2024-12-26 15:32:09 +0530

    hello.py modified

--- hello.py
+++ hello.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello():
     return "Welcome to the comedy class!!! Bapat is doing comedy with dhanno by calling him meetha"
 if __name__ == '__main__':
-     app.run(host='0.0.0.0',debug=True)
+     app.run(host='0.0.0.0', port='7000', debug=True)
 
 
 
